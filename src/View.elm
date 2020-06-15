d4e374bd0a28c518d794733a39c7d0366c08e224
YuukiAS <humc2013@163.com>
2020-06-15 09:54:38 +0800

    Version 0.92 correcting master branch

--- src/View.elm
+++ src/View.elm
@@ -1,78 +1,83 @@
 module View exposing (..)
-import Playground exposing (..)
-import Object
 
-type alias Axis =
-    {
-        top : Float
-        ,bot : Float
-        ,lef : Float
-        ,rig : Float
-    }
+import Object exposing (..)
+import Model exposing (..)
+import Message exposing (..)
+import Outlooks exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (style,src,controls,loop,autoplay,start)
+import List.Extra exposing (interweave,intercalate)
+import Svg exposing (svg)
+import Svg.Attributes exposing (viewBox)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (on, onClick, onMouseDown, onMouseUp)
+import Json.Decode as Json
+import Model exposing (Model)
+import Svg.Attributes as SvgAttrs
+import Dashboard exposing (..)
 
+view: Model -> Html Msg
+view model =
 
-view computer memory =
-    let
-        axis: Axis         --* 对type alias的赋值方法
-        axis = Axis computer.screen.top computer.screen.bottom computer.screen.left computer.screen.right
-    in
-    [
-        draw axis
-    ]
-
-
-draw: Axis -> Shape
-draw axis=
-     let
-        top = axis.top
-        bot = axis.bot
-        rig = axis.rig
-        lef = axis.lef
-        back_width = rig - lef
-        back_height = top - bot
-
-        pillar  = 3 -- 边框粗度
-        ratio = 1.85
-        pillar_width = 0.001*back_width*back_height    -- 边框宽度
-        pillar_height = pillar_width/ratio   -- 边框长度
-        brick_width = pillar_width*0.082
-        brick_height = pillar_width*0.035
-     in
-        group
+    div
+        [ style "backgroundColor" "#1d1d1d"
+        ]
         [
-         ------------------------------ Bricks
-            rectangle (rgb 255 255 255) back_width back_height
-        ,   rectangle (rgb 95 39 205) pillar_width pillar
-            |> moveDown (pillar_height*0.55)
-        ,   rectangle (rgb 95 39 205) pillar_width pillar
-            |> moveUp (pillar_height*0.45)
-        ,   rectangle (rgb 95 39 205)  pillar  (pillar_height*1.002)  -- 微调,影响不大
-            |> moveDown (pillar_height*0.05)
-            |> moveLeft (pillar_width/2)
-        ,   rectangle (rgb 95 39 205)  pillar  (pillar_height*1.002)
-            |> moveDown (pillar_height*0.05)
-            |> moveRight (pillar_width/2)
-        ,   words blue "Origin point"    -- 原点
-        ------------------------------ Others
-        ,   Object.ball (pillar_width*0.01)
-            |> moveDown (pillar_height*0.45)
-        ,   Object.paddle (pillar_width*0.12) (pillar_width*0.01)
-            |> moveDown (pillar_height*0.5)
-        ,   Object.grid brick_width brick_height (pillar_width*0.0832)
-            |> moveLeft (pillar_width*0.4575)
-            |> moveUp (pillar_height*0.415)
-        ,   Object.grid brick_width brick_height (pillar_width*0.0832)
-            |> moveLeft (pillar_width*0.4575)
-            |> moveUp (pillar_height*0.415- brick_height*1.02)
-        ,   Object.grid brick_width brick_height (pillar_width*0.0832)
-            |> moveLeft (pillar_width*0.4575)
-            |> moveUp (pillar_height*0.415- brick_height*2.04)
-        ,   Object.grid brick_width brick_height (pillar_width*0.0832)
-            |> moveLeft (pillar_width*0.4575)
-            |> moveUp (pillar_height*0.415- brick_height*3.06)
-         ,   Object.grid brick_width brick_height (pillar_width*0.0832)
-            |> moveLeft (pillar_width*0.4575)
-            |> moveUp (pillar_height*0.415- brick_height*4.08)
+            svg
+            [ viewBox "0 0 400 400" ]
+            (interweave  --? List.append会导致无法加载brick,但interweave可以
+            (List.append (List.append(List.append renderSettings (renderStatus model)) (renderRowBrick model (Point 10 8) 6.5 2.5 4 11)) (renderSkills model))
+            [
+                (renderBackground (Point 0 0) 100 65 outBkg),
+                (renderBackground (Point 9 8) 82 37 outInterface),
+                (renderBall (Point model.ball_x model.ball_y) 2 2 outBall),
+                (renderPaddle (Point model.pad_x 40) 12 1 outPaddle),
+                (renderDashboard (Point 40 0.5) 50 6)
+            ])
+
+         , div [][renderGameButton model]
+         , div [style "top" "700px" , style "left" "1000px", style "position" "absolute"][audio [src "src/assets/musics/Return of Ancients.mp3",controls True][]]
         ]
 
 
+
+
+
+renderGameButton : Model -> Html Msg
+renderGameButton model =
+    let
+        ( txt, msg ) =
+            case model.state of    --todo 这里命名有点混乱,我都改成开始/重开时需要按两次了(也就是初始为pause),应该可以把两个合并一下?
+                Model.Stopped ->
+                    ( "New game", Pause ) -- 刚开始是Pause
+
+                Model.Playing ->
+                    ( "Enjoy the game! You now have "++ Debug.toString model.life ++ " lives.", Keep )
+
+                Model.Paused ->
+                    if model.life > 1 then
+                    ( "Continue, you still have " ++ Debug.toString model.life ++ " lives.", Start )
+                    else if model.life == 1 then
+                    ( "Continue, you still have " ++ Debug.toString model.life ++ " life.", Start )
+                    else ("Game Over. Click here to restart.", Resume)
+    in
+    button
+        [ style "background" "#f8f4f4"
+                  , style "border" "0"
+                  , style "bottom" "30px"
+                  , style "color" "#0ca101"
+                  , style "cursor" "pointer"
+                  , style "display" "block"
+                  , style "font-family" "Helvetica, Arial, sans-serif"
+                  , style "font-size" "18px"
+                  , style "font-weight" "300"
+                  , style "height" "600x"
+                  , style "left" "330px"
+                  , style "line-height" "60px"
+                  , style "outline" "none"
+                  , style "padding" "0"
+                  , style "position" "absolute"
+                  , style "width" "600px"
+        , onClick msg
+        ]
+        [ text txt ]
