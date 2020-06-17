ad8695e0bc887086597b2c4d35d4c83b567f2501
YuukiAS <humc2013@163.com>
2020-06-18 00:36:20 +0800

    Version 0.97 - Sound effects and other modifications.

--- src/Calculate.elm
+++ src/Calculate.elm
@@ -4,14 +4,18 @@ module Calculate exposing (..)
 import Model exposing (Model,Keys)
 import Object exposing (r)
 
--- 计算坐标,与碰撞无关
+ballLeftCoordinate : Model -> (Int, Int)
+ballLeftCoordinate model =
+    (floor ((model.ball_y - 8)/4), floor ((model.ball_x - 3)/4))
 
---upCoordinate: Model -> (Int, Int)
---upCoordinate model = ( floor((model.ball_y - r - 8)/2.675), floor((model.ball_x - 10)/6.675) )
+ballRightCoordinate : Model -> (Int, Int)
+ballRightCoordinate model =
+    (floor ((model.ball_y - 8)/4), floor ((model.ball_x - 1)/4))
 
-leftCoordinate: Model -> (Int, Int)
-leftCoordinate model = (floor ((model.ball_y - 8) / 2.675), floor((model.ball_x - r - 8)/6.675))
+ballUpCoordinate : Model -> (Int, Int)
+ballUpCoordinate model =
+    (floor ((model.ball_y - 9)/4), floor ((model.ball_x - 2)/4))
 
-rightCoordinate: Model -> (Int, Int)
-rightCoordinate model = (floor ((model.ball_y - 8) / 2.675), floor((model.ball_x + r - 8)/6.675))
-
+ballDownCoordinate : Model -> (Int, Int)
+ballDownCoordinate model =
+    (floor ((model.ball_y - 7)/4), floor ((model.ball_x - 2)/4))
--- src/Outlooks.elm
+++ src/Outlooks.elm
@@ -15,6 +15,34 @@ outAddBall = "assets/main/addBall.png"
 outAddPaddle = "assets/main/addPaddle.png"
 outBoss = "assets/main/boss.png"
 
+background = "assets/main/bkp.png"
+interface = "assets/main/trans.png"
+ball = "assets/main/ball.png"
+vkernal5 = "assets/main/stage1.png"
+vkernal4 = "assets/main/stage2.png"
+vkernal3 = "assets/main/stage3.png"
+vkernal2 = "assets/main/stage4.png"
+vkernal1 = "assets/main/stage5.png"
+vkernal0 = "assets/main/gameover.png"
+pad = "assets/main/bendpaddle.png"
+gold = "assets/main/shootpaddle.png"
+attacker = "assets/main/attack.png"
+downleft = "assets/main/downleft.png"
+downright = "assets/main/downright.png"
+upleft = "assets/main/upleft.png"
+upright = "assets/main/upright.png"
+circular = "assets/main/circular.png"
+circular45 = "assets/main/diamond45.png"
+vkf = "assets/main/vkernelfrag.png"
+bright = "assets/main/brighten.png"
+leaf = "assets/main/brick.png"
+
+
+
+
+
+
+
 trait1 = "assets/skills/Trait_charismatic.png"
 trait2 = "assets/skills/Trait_cybernetic.png"
 trait3 = "assets/skills/Trait_very_cybernetic.png"
